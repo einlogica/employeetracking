971921870d4c14861ae30333fded8fef4e42ee55
Manjima94 <manji5570@gmail.com>
2025-08-20 16:45:56 +0530

    v3

--- lib/osrm_service.dart
+++ lib/osrm_service.dart
@@ -4,17 +4,16 @@ import 'package:http/http.dart' as http;
 import 'package:latlong2/latlong.dart';
 
 class OsrmService {
-  // You can change baseUrl to your own OSRM instance if needed.
+  
   static const String baseUrl = 'https://router.project-osrm.org';
 
-  /// Returns a polyline (list of LatLng) from OSRM between start and end.
   static Future<List<LatLng>> getRoute({
     required double startLat,
     required double startLng,
     required double endLat,
     required double endLng,
   }) async {
-    // OSRM expects lon,lat order in the path!
+    
     final path =
         '$baseUrl/route/v1/driving/$startLng,$startLat;$endLng,$endLat'
         '?overview=full&geometries=geojson&alternatives=false&steps=false';
@@ -30,7 +29,7 @@ class OsrmService {
     }
 
     final List coords = json['routes'][0]['geometry']['coordinates'];
-    // coords are [lon, lat]; convert to LatLng(lat, lon)
+   
     return coords.map<LatLng>((c) => LatLng(c[1].toDouble(), c[0].toDouble())).toList();
   }
 }
